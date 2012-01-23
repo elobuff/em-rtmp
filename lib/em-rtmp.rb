108784e21b1abb97cf4d17a550d2b8a6a5557488
Jason Coene <jcoene@gmail.com>
2012-01-23 17:36:39 -0500

    Add base module with exceptions

--- lib/em-rtmp.rb
+++ lib/em-rtmp.rb
@@ -1,2 +1,14 @@
 require "eventmachine"
+
+require "em-rtmp/buffer"
+require "em-rtmp/connection"
 require "em-rtmp/version"
+
+module EventMachine
+  module RTMP
+
+    class RTMPError < RuntimeError; end
+    class HandshakeError < RTMPError; end
+
+  end
+end

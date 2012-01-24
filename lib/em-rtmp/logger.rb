c9222d12217c9bf329b2cdd5da4b4f058ce57666
Jason Coene <jcoene@gmail.com>
2012-01-23 21:09:01 -0500

    Improve readability of Logger

--- lib/em-rtmp/logger.rb
+++ lib/em-rtmp/logger.rb
@@ -20,9 +20,21 @@ module EventMachine
         log 'ERROR', message
       end
 
-      def self.print(message)
-        log message
-        puts message
+      def self.print(message, options={})
+        @@previous_indent ||= 0
+
+        options[:caller] ||= caller
+        options[:indent] ||= 0
+        caller_splat = options[:caller][0].split(":")
+        ruby_file = caller_splat[0].split("/").last
+        ruby_line = caller_splat[1]
+        ruby_method = caller_splat[2].match(/`(.*)'/)[1]
+
+        puts "" if @@previous_indent != options[:indent]
+        indent = ">" * (options[:indent] * 2)
+        puts "%s%-#{30-indent.length}s%-30s%s" % [indent, "#{ruby_file}:#{ruby_line}", ruby_method, message]
+
+        @@previous_indent = options[:indent]
       end
 
     end

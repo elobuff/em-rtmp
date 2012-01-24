b97e3ae5390c61374c161316031aebe24784b78c
Jason Coene <jcoene@gmail.com>
2012-01-24 14:42:52 -0500

    Nix the logger while testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
 require "rubygems"
 require "bundler/setup"
 require "em-rtmp"
+
+module EventMachine
+  module RTMP
+    class Logger
+      def self.log(l, m); end
+      def self.debug(m); end
+      def self.info(m); end
+      def self.error(m); end
+      def self.print(m, o={}); end
+    end
+  end
+end
